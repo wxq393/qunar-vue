04202fa8b42b56f3d9f21dcbd416801db7cd0bb1
wxq393 <wxq393@wxq393deMacBook-Pro.local>
2019-01-18 01:01:06 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'  
+}   
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    // 垂直居中
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute       
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .44rem
+      text-align: center
+      color: $bgColor
+</style>
+
+
+
+
+
+
+
