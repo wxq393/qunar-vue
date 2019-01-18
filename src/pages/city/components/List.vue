2399c458dd78efae835aa759e84d539ad70ad16f
wxq393 <wxq393@wxq393deMacBook-Pro.local>
2019-01-18 16:52:47 +0800

    ajax city

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -20,83 +20,26 @@
     <div class="area">
       <div class="title" border-topbottom>热门城市</div>
       <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+        <div 
+          class="button-wrapper" 
+          v-for="item of hot"
+          :key="item.id"
+        >
+          <div class="button">{{item.name}}</div>
         </div>
       </div>
     </div>
  
-    <div class="area">
-      <div class="title border-topbottom">A</div>
+    <div class="area" v-for="(item, key) of cities" :key="key">
+      <div class="title border-topbottom">{{key}}</div>
       <div class="item-list">
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
-        <div class="item border-bottom">阿尔山</div>
+        <div 
+          class="item border-bottom"
+          v-for="innerItem of item"
+          :key="innerItem.id"
+        >
+          {{innerItem.name}}
+        </div>
       </div>
     </div>
 
@@ -108,6 +51,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   } 
