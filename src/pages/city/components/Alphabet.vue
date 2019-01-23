6db4a5f6e26bfad5b2ca3d5ee0754292e8f58957
wxq393 <wxq393@wxq393deMacBook-Pro.local>
2019-01-23 16:30:05 +0800

    performance

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,17 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+    <li 
+      class="item" 
+      v-for="item of letters" 
+      :key="item"
+      :ref="item"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      @click="handleLetterClick"
+    >
+      {{item}}
+    </li>
   </ul>
 </template>
 
@@ -9,6 +20,50 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for(let i in this.cities) {
+         letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout (()=>{
+          const touchY = e.touches[0].clientY - 74
+          const index = Math.floor((touchY - this.startY) / 22 )
+          if(index >= 0 && index < this.letters.length){
+          this.$emit('change', this.letters[index])
+          }   
+        },16)             
+      }    
+    },
+    handleTouchEnd () {
+        this.touchStatus = false
+    }
   }  
 }   
 </script>
@@ -16,7 +71,6 @@ export default {
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
   .list
-    // 垂直居中
     display: flex
     flex-direction: column
     justify-content: center
